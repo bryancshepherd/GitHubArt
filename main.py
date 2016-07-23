98dce6205f1070250ca79cff26f2bd001ee2cf56
Bryan Shepherd <bryan.shepherd@windsorcircle.com>
2016-07-22 21:28:29 -0400

    Create directory for text files.

--- main.py
+++ main.py
@@ -4,7 +4,30 @@ Created on Fri Jul 22 16:59:38 2016
 
 """
 
-from github import Github
+import github3 # docs here: http://github3py.readthedocs.io/en/latest/examples/github.html
+import csv
+import os
 
-with auth.csv
+os.chdir('/Users/bryanshepherd/Projects/GitHubTyper/')
 
+with open('auth/auth.csv', newline='') as f:
+    text = csv.reader(f)
+
+    for row in text:
+        user_name, password = row
+
+g = github3.login(user_name, password)
+
+for repo in g.get_user().get_repos():
+    print(repo.name)
+
+help(Github)
+
+g.get_rate_limit().rate.remaining
+
+help(github)
+
+
+g_com = g.Commit()
+
+help(github.GitObject)
